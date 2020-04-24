230a08eed247a8d575838533313494587fd40526
matmaly <mateuszfmaly@gmail.com>
2020-04-24 15:41:30 +0000

    Adding a print fun which prints the results

--- collatz_conjecture/collatz_conjecture.py
+++ collatz_conjecture/collatz_conjecture.py
@@ -3,20 +3,22 @@
 Author: Matuesz Maly
 CollatzConjecture algorithm implemented in Python
 '''
-class collatzConjecture():
+class CollatzConjecture():
     def __init__(self, n):
         self.n = n
         counter = 0
         sequence = []
    
-        print(self.compute(self.n,counter,sequence))
+        n, counter, sequence = self.compute(self.n,counter,sequence)
+        
+        self.print_result(n, counter, sequence)
 
     def compute(self, n, counter, sequence):
 
         if n == 1:
             counter += 1
             sequence.append(n)
-            return f"Reached:  {n}\nAfter {counter} loops!\nSequence: {sequence}"
+            return n, counter, sequence
         elif n % 2 ==0:
             counter += 1
             sequence.append(n)
@@ -25,6 +27,14 @@ class collatzConjecture():
             counter += 1
             sequence.append(n)
             return self.compute((n * 3 + 1), counter, sequence)
+
+    def print_result(self, n, counter, sequence):
+        self.n = n
+        self.counter = counter
+        self.sequence = sequence
+
+        print(f"Reached {self.n} after {self.counter} loops!")
+        print("Sequence: ", *self.sequence)
 
 collatzConjecture(int(input("Enter a number: ")))
 
