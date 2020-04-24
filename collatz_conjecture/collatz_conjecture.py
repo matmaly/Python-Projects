96d2bdf5ae7629594104648b2cacbfcc12fe534a
matmaly <mateuszfmaly@gmail.com>
2020-04-24 15:28:47 +0000

    Modifying the fun to include the sequence of the numbers to get to 1

--- collatz_conjecture/collatz_conjecture.py
+++ collatz_conjecture/collatz_conjecture.py
@@ -7,20 +7,24 @@ class collatzConjecture():
     def __init__(self, n):
         self.n = n
         counter = 0
+        sequence = []
    
-        print(self.compute(self.n,counter))
+        print(self.compute(self.n,counter,sequence))
 
-    def compute(self, n, counter):
+    def compute(self, n, counter, sequence):
 
         if n == 1:
             counter += 1
-            return f"Reached:  {n}\nAfter {counter} loops!"
+            sequence.append(n)
+            return f"Reached:  {n}\nAfter {counter} loops!\nSequence: {sequence}"
         elif n % 2 ==0:
             counter += 1
-            return self.compute((n // 2), counter)
+            sequence.append(n)
+            return self.compute((n // 2), counter, sequence)
         else:
             counter += 1
-            return self.compute((n * 3 + 1), counter)
+            sequence.append(n)
+            return self.compute((n * 3 + 1), counter, sequence)
 
 collatzConjecture(int(input("Enter a number: ")))
 
